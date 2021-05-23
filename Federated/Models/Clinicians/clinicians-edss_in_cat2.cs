e65593756733e12eb70e46a383f0ca0266feae11
Ashkan Pirmani <ashkan_pirmani@outlook.com>
2021-05-23 17:11:56 +0200

    Tuning the output variables+

--- Federated/Models/Clinicians/clinicians-edss_in_cat2.cs
+++ Federated/Models/Clinicians/clinicians-edss_in_cat2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,10 +8,13 @@ namespace Federated.Models.Clinicians
 {
     public class clinicians_edss_in_cat2
     {
+        [JsonProperty("0")]
         public int zero { get; set; }
+        [JsonProperty("1")]
         public int one { get; set; }
-        public int missing { get; set; }
 
+        public int missing { get; set; }
+        [JsonProperty("2")]
         public int two { get; set; }
 
 
--- Federated/Models/Patients/patients-edss_in_cat2.cs
+++ Federated/Models/Patients/patients-edss_in_cat2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,11 +8,15 @@ namespace Federated.Models.Patients
 {
     public class patients_edss_in_cat2
     {
+        [JsonProperty("0")]
         public int zero { get; set; }
+        [JsonProperty("1")]
         public int one { get; set; }
-        public int missing { get; set; }
 
+        public int missing { get; set; }
+        [JsonProperty("2")]
         public int two { get; set; }
+
 
     }
 }
